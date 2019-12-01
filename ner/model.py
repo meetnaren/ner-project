7701608abd299ea26a81225eec17fb8748402c13
happilyeverafter95 <mgu@wealthsimple.com>
2019-11-30 22:38:02 -0500

    set up db engine

--- ner/model.py
+++ ner/model.py
@@ -1,7 +1,14 @@
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+
 from ner import settings
+from ner.db import DB
 
 print(settings.env)
 
 
 class Model:
-    pass
+    @staticmethod
+    def load_data(sql):
+        sessionmaker(bind=DB.create_db_engine(), autocommit=True)
+        return pd.read_sql_query(sql, con=DB.create_db_engine())
