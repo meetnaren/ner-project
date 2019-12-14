da0a9957849833514078ba43a611cd72b0f895e5
mandy <yymgu@edu.uwaterloo.ca>
2019-12-13 22:25:26 -0500

    add key

--- ner/model.py
+++ ner/model.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 from sqlalchemy.orm import sessionmaker
 
-from ner import settings
 from ner.db import DB
 from ner.datawarehouse.tables import ExtractedEntities
 
 
 class NER:
-    def __init__(self):
+    def __init__(self, id):
         session = sessionmaker(bind=DB.create_db_engine())
+        self.id = id
         self.session = session()
         self.created_at = datetime.datetime.today().date()
 
@@ -24,11 +24,12 @@ class NER:
 
     def store_ner(self, text, entities):
         ner = ExtractedEntities(
+                id=self.id,
                 created_at=self.created_at,
                 text=text,
                 entities=entities)
         self.session.add(ner)
         self.session.commit()
-    
+
     def close_session(self):
         self.session.close()
--- ner/routers/ner.py
+++ ner/routers/ner.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 @router.post('/extract_name')
 def extract_name(input: dict):
-    ner = NER()
+    ner = NER(input['id'])
     result = ner.placeholder_method(input['text'])
     ner.store_ner(input['text'], result)
     return JSONResponse({'result': result})
