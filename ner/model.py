c43dd66807e638fca095ccdff0a7dec0915532d6
Naren <meetnaren@gmail.com>
2020-06-27 14:25:12 -0500

    inference changes

--- ner/model.py
+++ ner/model.py
@@ -1,10 +1,24 @@
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
+import logging
 
 from ner import settings
 from ner.db import DB
 
 print(settings.env)
+
+def extract_entities(input_text):
+    # get latest model
+    latest_model = max([d for d in os.listdir('Models')])
+    nlp = spacy.load('Models/'+latest_model)
+    logging.debug(f'Loaded model {latest_model}')
+
+    # apply latest model on input text
+    doc = nlp(input_text)
+
+    # return the entities and texts
+    result = [(ent.label_, ent.text) for ent in doc.ents] 
+    return result
 
 
 class NER:
