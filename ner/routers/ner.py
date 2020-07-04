4214a2c8414cbe7ed634f90ce83118dd8e2bcd9b
happilyeverafter95 <mgu@wealthsimple.com>
2020-07-03 23:04:24 -0400

    specify input type + mount

--- ner/routers/ner.py
+++ ner/routers/ner.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter
 from ner.model import NER
 from starlette.responses import JSONResponse
+from pydantic import BaseModel
 
 
 router = APIRouter()
 
 
+class NameExtractionInput(BaseModel):
+    text: str
+
+
 @router.post('/extract_name')
-def extract_name(input: dict):
+def extract_name(input: NameExtractionInput) -> JSONResponse:
     ner = NER()
-    result = ner.placeholder_method(input['text'])
+    result = ner.placeholder_method(input.text)
     return JSONResponse({'result': result})
