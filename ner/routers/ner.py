83c4a44f1dbc0d394ac6e0888dd59f89937bda07
meetnaren <meetnaren@gmail.com>
2020-08-03 00:41:23 -0500

    changes to ner.py as suggested by circleCI

--- ner/routers/ner.py
+++ ner/routers/ner.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 from ner.model import extract_entities
 from starlette.responses import JSONResponse
-import os
-import spacy
 
 router = APIRouter()
+
 
 @router.post('/recognize_entities')
 def recognize_entities(input: dict):
