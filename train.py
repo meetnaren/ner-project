1f9c48e35c82bf5601f0cb28481a085bfb1f29bd
meetnaren <meetnaren@gmail.com>
2020-08-05 10:29:42 -0500

    changes to train.py to augment names

--- train.py
+++ train.py
@@ -15,31 +15,37 @@ import copy
 
 names = pd.read_csv('top100names.csv')
 
+
 def send_output(text):
     if __name__ == '__main__':
         print(text)
     else:
         logging.info(text)
 
+
 def get_random_name():
-    random_row = names[names['type']=='f'].sample(1)
+    random_row = names[names['type'] == 'f'].sample(1)
     c = random_row.iloc[0]['country']
     first_name = random_row.iloc[0]['name']
-    last_name = names[(names['country']==c) & (names['type']=='s')].sample(1).iloc[0]['name']
+    last_name = names[(names['country'] == c) & (names['type'] == 's')]\
+        .sample(1).iloc[0]['name']
 
     return (first_name, last_name)
 
+
 def replace_name(data_tuple, first_name, last_name):
     text = data_tuple[0]
-    entities = sorted(copy.deepcopy(data_tuple[1]['entities']), key=lambda x:x[0])
-    
+    entities = sorted(copy.deepcopy(data_tuple[1]['entities']),
+                      key=lambda x: x[0])
+
     for i in range(len(entities)):
         entity_type = entities[i][2]
         if entity_type == 'FIRST_NAME':
             new_string = first_name
         elif entity_type == 'LAST_NAME':
             new_string = last_name
-        else: continue
+        else:
+            continue
         old_string_len = entities[i][1] - entities[i][0]
         new_string_len = len(new_string)
         diff = new_string_len - old_string_len
@@ -61,7 +67,8 @@ def replace_name(data_tuple, first_name, last_name):
         send_output(text)
         send_output(entities)
         raise AssertionError
-    return (text.lower(), {'entities':entities})
+    return (text.lower(), {'entities': entities})
+
 
 def get_train_data(labels, data_aug_factor):
     with open('dump.json', 'r') as f:
@@ -71,7 +78,7 @@ def get_train_data(labels, data_aug_factor):
     data = []
 
     for c, v in tqdm(data_dump.items()):
-        data_tuple = (v['text'].lower(), {'entities':[]})
+        data_tuple = (v['text'].lower(), {'entities': []})
         if v['entities']:
             if any([i in [e[2] for e in v['entities']] for i in labels]):
                 for e in v['entities']:
@@ -82,8 +89,9 @@ def get_train_data(labels, data_aug_factor):
                     data.append(replace_name(data_tuple, *(get_random_name())))
 
     send_output(f'{len(data)} records added.')
-        
+
     return data
+
 
 def evaluate(ner_model, examples):
     scorer = Scorer()
@@ -94,9 +102,11 @@ def evaluate(ner_model, examples):
         scorer.score(pred_value, gold)
     return scorer.scores
 
+
 def main(model='latest', n_iter=10):
-#model: 'latest' to retrieve the latest model from the Models directory, 'blank' to train a blank model from scratch
-#n_iter: no. of iterations to train the model
+    # model: 'latest' to retrieve the latest model from the Models directory,
+    # 'blank' to train a blank model from scratch
+    # n_iter: no. of iterations to train the model
     LABELS = [
         'FIRST_NAME',
         'LAST_NAME'
@@ -129,8 +139,8 @@ def main(model='latest', n_iter=10):
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
 
     with nlp.disable_pipes(*other_pipes) and warnings.catch_warnings():
-        #show warnings for misaligned entity spans once
-        warnings.filterwarnings("ignore", category=UserWarning, module='spacy')
+        # show warnings for misaligned entity spans once
+        warnings.filterwarnings('ignore', category=UserWarning, module='spacy')
 
         sizes = compounding(1.0, 4.0, 1.001)
         # batch up the examples using spaCy's minibatch
@@ -139,30 +149,33 @@ def main(model='latest', n_iter=10):
             random.shuffle(DATA)
             TRAIN_DATA = DATA[:int(TRAIN_VAL_SPLIT * num_records)]
             VALID_DATA = DATA[len(TRAIN_DATA):]
-            send_output(f'Training on {len(TRAIN_DATA)} records, validating on {len(VALID_DATA)} records.')
+            send_output(f'Training on {len(TRAIN_DATA)} records, \
+                validating on {len(VALID_DATA)} records.')
 
             batches = minibatch(TRAIN_DATA, size=sizes)
             losses = {}
             for batch in tqdm(batches):
                 texts, annotations = zip(*batch)
-                nlp.update(texts, annotations, sgd=optimizer, drop=0.35, losses=losses)
-            send_output("Training losses: " + str(losses))
+                nlp.update(texts, annotations, sgd=optimizer, drop=0.35,
+                           losses=losses)
+            send_output('Training losses: ' + str(losses))
             scores = evaluate(nlp, VALID_DATA)
             send_output(scores)
-        
+
     # test the trained model
-    test_text = "The name of my mentor is Mandy Gu"
+    test_text = 'The name of my mentor is Mandy Gu'
     doc = nlp(test_text)
     send_output(f'Entities in {test_text}')
     for ent in doc.ents:
         send_output(ent.label_ + ' ' + ent.text)
 
-    #save the trained model
-    model_name = 'model_' + time.strftime("%Y%m%d-%H%M%S")
+    # save the trained model
+    model_name = 'model_' + time.strftime('%Y%m%d-%H%M%S')
     nlp.meta['name'] = model_name
     nlp.meta['author'] = 'Naren Santhanam'
     nlp.meta['email'] = 'meetnaren@gmail.com'
     nlp.to_disk(f'Models/{model_name}')
+
 
 if __name__ == '__main__':
     main()
